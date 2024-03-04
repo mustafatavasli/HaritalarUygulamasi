0eb53b34c1cfbc12d1aaba2305448ee7c34fcd0e
mustafatavasli <musstafatavasli@gmail.com>
2024-03-04 18:21:47 +0300

    Initial Commit

---
+++ HaritalarUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaritalarUygulamasi
+//
+//  Created by Mustafa TAVASLI on 4.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
